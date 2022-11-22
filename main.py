f7d05721fbd54dd453f7aca90b919326d871216d
Harald Theodor Helland Velde <94077617+MrAbiotic@users.noreply.github.com>
2022-11-22 08:24:41 +0100

    La til en Card-klasse for alle kort

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+class Card:
+    """
+    Lager ett kort som er 1-indexed (ess = 1, 2 = 2, osv.)
+    """
+    def __init__(self, card_num: int, suit: str) -> None:
+        self.card_num = card_num
+        self.suit = suit
+
+    def card_value(self) -> int:
+        if 2 <= self.card_num <= 10:
+            return self.card_num
+
+
 print("Hello world")
